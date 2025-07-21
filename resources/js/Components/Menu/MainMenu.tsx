953f0c719ea3448befbfe245b4d61c44b3a924d2
mohamed lamine <mohamed61lamine@gmail.com>
2025-07-21 01:19:27 +0100

    `Added react-icons package and updated MainMenu component to use Flowbite and React Icons`

--- resources/js/Components/Menu/MainMenu.tsx
+++ resources/js/Components/Menu/MainMenu.tsx
@@ -1,5 +1,10 @@
 import MainMenuItem from '@/Components/Menu/MainMenuItem';
 import { Building, CircleGauge, Printer, Users } from 'lucide-react';
+import { Accordion, AccordionContent, AccordionPanel, AccordionTitle } from "flowbite-react";
+
+import { Sidebar, SidebarCollapse, SidebarItem, SidebarItemGroup, SidebarItems } from "flowbite-react";
+import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser } from "react-icons/hi";
+
 
 interface MainMenuProps {
   className?: string;
@@ -8,7 +13,7 @@ interface MainMenuProps {
 export default function MainMenu({ className }: MainMenuProps) {
   return (
     <div className={className}>
-      <MainMenuItem
+      {/* <MainMenuItem
         text="Dashboard"
         link="dashboard"
         icon={<CircleGauge size={20} />}
@@ -28,6 +33,38 @@ export default function MainMenu({ className }: MainMenuProps) {
         link="reports"
         icon={<Printer size={20} />}
       />
+       */}
+
+       <Sidebar className='fixed z-40 top-16 left-0 md:left-0 lg:left-0 w-full md:w-56 lg:w-56 h-screen transition-transform  bg-white border-r border-gray-200 translate-x-0 duration-500'>
+          <SidebarItems>
+            <SidebarItemGroup>
+              <SidebarItem href="#" icon={HiChartPie}>
+                Dashboard
+              </SidebarItem>
+              <SidebarCollapse icon={HiShoppingBag} label="E-commerce">
+                <SidebarItem href="#">Products</SidebarItem>
+                <SidebarItem href="#">Sales</SidebarItem>
+                <SidebarItem href="#">Refunds</SidebarItem>
+                <SidebarItem href="#">Shipping</SidebarItem>
+              </SidebarCollapse>
+              <SidebarItem href="#" icon={HiInbox}>
+                Inbox
+              </SidebarItem>
+              <SidebarItem href="#" icon={HiUser}>
+                Users
+              </SidebarItem>
+              <SidebarItem href="#" icon={HiShoppingBag}>
+                Products
+              </SidebarItem>
+              <SidebarItem href="#" icon={HiArrowSmRight}>
+                Sign In
+              </SidebarItem>
+              <SidebarItem href="#" icon={HiTable}>
+                Sign Up
+              </SidebarItem>
+            </SidebarItemGroup>
+          </SidebarItems>
+        </Sidebar>
     </div>
   );
 }
